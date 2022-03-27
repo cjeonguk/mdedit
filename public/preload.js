bdb9ac99b7e231af3f6aba1240c875e4c16ab27a
Jeonguk Choi <wooram9816@gmail.com>
2022-03-27 10:51:19 +0900

    fix(save): unzip and use tmpdir

--- public/preload.js
+++ public/preload.js
@@ -1,6 +1,8 @@
 const { contextBridge, ipcRenderer } = require('electron');
 const fs = require('fs');
+const os = require('os');
 const JSZip = require('jszip');
+const path = require('path');
 
 ipcRenderer.on('saveFile', (event, [filePath, images]) => {
   let contents = document.getElementById('tArea').value;
@@ -8,20 +10,46 @@ ipcRenderer.on('saveFile', (event, [filePath, images]) => {
     const filePathSplit = filePath.split('/');
     const fileName = filePathSplit[filePathSplit.length - 1].slice(0, -4);
     const zip = new JSZip();
-    const photoZip = zip.folder('photos');
     for (let i in images) {
       const imageSplit = images[i].split('/');
       const image = imageSplit[imageSplit.length - 1];
-      photoZip.file(image, fs.readFileSync(images[i]));
-      contents = contents.replace(images[i], `photos/${image}`);
+      zip.file(image, fs.readFileSync(images[i]));
     }
     zip.file(`${fileName}.md`, contents);
+    zip.file('images.txt', images.toString());
     document.getElementById('tArea').value = contents;
     zip
       .generateNodeStream({ type: 'nodebuffer', streamFiles: true })
-      .pipe(fs.createWriteStream(`${filePath}`))
+      .pipe(fs.createWriteStream(filePath))
       .on('finish', () => {
-        console.log('zip written.');
+        const dest = [];
+        fs.readFile(filePath, (err, data) => {
+          if (err) throw err;
+          fs.mkdtemp(path.join(os.tmpdir(), 'md_img-'), (err, directory) => {
+            zip.loadAsync(data).then((zip) => {
+              Object.keys(zip.files).forEach((readFileName) => {
+                zip.files[readFileName].async('nodebuffer').then((fileData) => {
+                  dest.push(path.join(directory, readFileName));
+                  fs.writeFileSync(dest[dest.length - 1], fileData);
+                });
+              });
+            });
+            setTimeout(() => {
+              const imgDest = dest.filter((word) => {
+                return (
+                  word.slice(word.length - 2) != 'md' &&
+                  word.slice(word.length - 3) != 'txt'
+                );
+              });
+              for (let i in imgDest) {
+                document.getElementById('tArea').value = contents.replace(
+                  images[i],
+                  imgDest[i]
+                );
+              }
+            }, 1000);
+          });
+        });
       });
   } else {
     fs.writeFileSync(filePath, contents, 'utf8');
